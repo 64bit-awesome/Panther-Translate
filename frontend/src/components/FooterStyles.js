ed338ccc39f22ca1a8ea7e576b65677da8f73223
Jordy Araujo <jordy.araujo@hotmail.com>
2021-11-17 08:04:32 -0500

    Remove unused css

--- frontend/src/components/FooterStyles.js
+++ frontend/src/components/FooterStyles.js
@@ -24,25 +24,6 @@ export const Container = styled.div`
     max-width: 1000px;
     margin: 0 auto;
     /* background: red; */
-`
-   
-export const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  margin-left: 60px;
-`;
-   
-export const Row = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 
-                         minmax(185px, 1fr));
-  grid-gap: 20px;
-   
-  @media (max-width: 1000px) {
-    grid-template-columns: repeat(auto-fill, 
-                           minmax(200px, 1fr));
-  }
 `;
    
 export const FooterLink = styled.a`
@@ -55,11 +36,4 @@ export const FooterLink = styled.a`
       color: yellow;
       transition: 200ms ease-in;
   }
-`;
-   
-export const Heading = styled.p`
-  font-size: 24px;
-  color: #fff;
-  margin-bottom: 40px;
-  font-weight: bold;
 `;
